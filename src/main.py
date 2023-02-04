7d3007564db7acb752112cdb110cf50fa75fe11f
Maksym Lopatin <holywag@gmail.com>
2023-02-04 20:13:54 +0200

    Implement asyncio loop execution

--- src/main.py
+++ src/main.py
@@ -8,53 +8,114 @@ import os
 import model
 import filters
 import itertools
+import random
+import traceback
+
+class FakeConfiguration:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def __repr__(self):
+        return f'FakeConfiguration("{self.file_path}")'
+
+class FakeStmt:
+    def __init__(self, stmt, conf):
+        self.conf = conf
+        self.stmt = stmt
+    
+    def __repr__(self):
+        return f'FakeStmt(conf="{self.conf}", stmt="{self.stmt}")'
+
+class FakeObserver(rx.typing.Observer):
+    def on_next(self, value):
+        logging.debug(f'FakeObserver: on_next({value})')
+
+    def on_completed(self):
+        logging.debug('FakeObserver: on_completed()')
+
+    def on_error(self, error):
+        logging.error(f'FakeObserver: on_error({error})')
+
+
+class UploadObserver(rx.typing.Observer):
+    def __init__(self):
+        self.transactions = []
+
+    async def send_transactions(self):
+        sleep_duration = random.randrange(2, 5)
+        logging.info(f'UploadObserver: send_transactions sleep_duration={sleep_duration} n={len(self.transactions)} {self.transactions}')
+        await asyncio.sleep(sleep_duration)
+        logging.info(f'UploadObserver: send_transactions DONE')
+
+    def on_next(self, transaction):
+        logging.debug(f'UploadObserver on_next {transaction}')
+        self.transactions.append(transaction)
+    
+    def on_completed(self):
+        logging.debug(f'UploadObserver on_completed')
+        asyncio.create_task(self.send_transactions())
+
+    def on_error(self, error):
+        logging.error(f'UploadObserver: on_error({error})')
 
 class RequestEngine:
     def __init__(self):
-        self.handlers = []
+        self.observers = []
 
-    def add_transaction_handler(self, handler):
-        self.handlers.append(handler)
-
-    def __execute_handlers(self, t):
-        for handler in self.handlers:
-            handler(t)
-
-    @staticmethod
-    def __async_call(task_func):
-        def __call_async_func(*args, **kwargs):
-            return rx.from_future(asyncio.create_task(task_func(*args, **kwargs)))
-        return __call_async_func
+    def add_transaction_observer(self, observer):
+        self.observers.append(observer)
 
     async def __request_statements(self, configuration):
+        # TODO: implement real logic of request
         # TODO: add configuration to statement/transaction objects
-        pass
+        sleep_duration = random.randrange(1, 10)
+        logging.info(f'request_statements: sleep_duration={sleep_duration} configuration={configuration}')
+        await asyncio.sleep(sleep_duration)
+        logging.info(f'request_statements: configuration={configuration} DONE')
+        return rx.from_list([
+            FakeStmt('stmt1', configuration),
+            FakeStmt('stmt2', configuration),
+            FakeStmt('stmt3', configuration)])
 
-    async def run(self, configs_dir):
-        rx.from_iterable(os.listdir(configs_dir)) \
-            .pipe(
-                op.map(lambda p: os.path.join(configs_dir, p)),
-                op.filter(os.path.isdir),
-                op.map(model.configuration.Configuration),
-                op.flat_map(self.__async_call(self.__request_statements)),
-                op.retry(3),
-                op.map(model.monobank_statement.BankStatement),
-                op.filter(filters.TransferFilter()),
-                op.map(model.ynab_transaction.YnabTransactionConverter()),
-                op.do_action(on_next = self.__execute_handlers),
-            ) \
-            .subscribe(
-                on_next=lambda trans: asyncio.ensure_future(send_transactions(trans)),
-                on_completed=lambda: await asyncio.sleep(10),
-                scheduler=AsyncIOScheduler(asyncio.get_event_loop())
-            )
+    async def __main(self):
+        # conf_observable = rx.from_iterable(os.listdir(configs_dir)) \
+        #     .pipe(
+        #         op.map(lambda p: os.path.join(configs_dir, p)),
+        #         op.filter(os.path.isdir),
+        #         # op.map(model.configuration.Configuration),
+        #         op.map(FakeConfiguration),
+        rx.from_list([
+            FakeConfiguration('file_path1'),
+            FakeConfiguration('file_path2')
+        ]) \
+        .pipe(
+            op.flat_map(lambda conf: rx.from_future(asyncio.create_task(self.__request_statements(conf)))),
+            op.merge_all(),
+            # op.retry(3),
+            # # op.filter(filters.TransferFilter()),
+            # # op.map(model.ynab_transaction.YnabTransactionConverter()),
+            *(op.do(o) for o in self.observers)
+        ) \
+        .subscribe(
+            scheduler=AsyncIOScheduler(asyncio.get_running_loop())
+        )
+
+    def run(self):
+        logging.info('Creating loop')
+        loop = asyncio.new_event_loop()
+        loop.create_task(self.__main())
+        aws = asyncio.all_tasks(loop)
+        logging.info('Running loop tasks')
+        while len(aws) > 0:
+            loop.run_until_complete(asyncio.gather(*aws))
+            aws = asyncio.all_tasks(loop)
 
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
-        level=logging.INFO,
+        level=logging.DEBUG,
         datefmt='%Y-%m-%d %H:%M:%S')
 
-    # loop the scheduler
-    asyncio.run(main(importers))
-    
+    engine = RequestEngine()
+    engine.add_transaction_observer(UploadObserver())
+    engine.run()
