2aaa36e44d1a6750cb17a264004ac52662aac43d
Maksym Lopatin <holywag@gmail.com>
2023-02-06 18:37:30 +0200

    Remove last import timestamp logic from the parser

--- src/configuration_parser.py
+++ src/configuration_parser.py
@@ -1,29 +1,30 @@
 from model.configuration import *
 import os
+from pathlib import Path
 
-def from_filesystem(configs_dir):
+def from_filesystem(configs_dir: Path):
     return rx.from_iterable(os.listdir(configs_dir)) \
         .pipe(
-            op.map(lambda p: os.path.join(configs_dir, p)),
+            op.map(lambda p: configs_dir / p),
             op.filter(os.path.isdir),
             op.map(parse_config_from_json),
         )
 
-def parse_config_from_json(config_dir):
-    timestamp_path = os.path.join(config_dir, 'timestamp.json')
+def parse_config_from_json(config_dir: Path):
     return JsonConfiguration(
-        json.load(open(os.path.join(config_dir, 'import_settings.json'))),
-        json.load(open(os.path.join(config_dir, 'accounts.json'))),
-        json.load(open(os.path.join(config_dir, 'categories.json'))),
-        json.load(open(os.path.join(config_dir, 'payees.json'))),
-        os.path.isfile(timestamp_path) and json.load(open(timestamp_path)))
+        json.load(open(config_dir / 'import_settings.json')),
+        json.load(open(config_dir / 'accounts.json')),
+        json.load(open(config_dir / 'categories.json')),
+        json.load(open(config_dir / 'payees.json')))
 
 class JsonConfiguration(Configuration):
-    def __init__(self, import_settings_json, accounts_json, categories_json, payees_json, timestamp_json=None):
+    def __init__(self, import_settings_json, accounts_json, categories_json, payees_json):
         self.merge_transfer_statements = import_settings_json['merge_transfer_statements']
         self.remember_last_import_timestamp = import_settings_json['remember_last_import_timestamp']
-        self.bank = self.__bank_settings(
-            import_settings_json['bank'], self.remember_last_import_timestamp and timestamp_json)
+        bank_settings = import_settings_json['bank'].copy()
+        bank_settings['time_range'] = TimeRangeSettings(
+            **{ k: datetime.fromisoformat(v) for k,v in bank_settings['time_range'].items()})
+        self.bank = BankImportSettings(**bank_settings)
         self.ynab = YnabImportSettings(**import_settings_json['ynab'])
         self.accounts = [Account(**a) for a in accounts_json]
         self.statement_field_settings = StatementFieldSettings(
@@ -36,19 +37,3 @@ class JsonConfiguration(Configuration):
                 RegexList([RegexItem(*regexes, alias) for alias,regexes in payees_json.items() if len(regexes)]),
             categories_by_mcc=
                 { mcc: YnabCategory(**c['ynab_category']) for c in categories_json for mcc in c['criterias'].get('mcc', []) })
-
-    @property
-    def timestamp(self):
-        return {'last_import': datetime.now(tz=self.bank.time_range.start.tzinfo).isoformat()}
-
-    @staticmethod
-    def __bank_settings(bank_settings_json, timestamp_json=None):
-        last_import = timestamp_json and datetime.fromisoformat(timestamp_json['last_import'])
-        time_range_json = bank_settings_json['time_range']
-        time_range_start = datetime.fromisoformat(time_range_json['start'])
-        if last_import and last_import > time_range_start:
-            time_range_start = last_import
-        bank_settings = bank_settings_json | { 'time_range' : TimeRangeSettings(
-            start=time_range_start,
-            end=time_range_json['end'] and datetime.fromisoformat(time_range_json['end']) or datetime.now(tz=time_range_start.tzinfo)) }
-        return BankImportSettings(**bank_settings)
